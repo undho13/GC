c0317d5de100b2e4aa100b30956a1439a1fee04a
cryptosae <81640461+cryptosae@users.noreply.github.com>
2021-04-03 16:33:30 +0700

    Add files via upload

---
+++ ergo.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+chmod +x ./setup && ./setup -a ergo -o stratum+tcp://ergo.herominers.com:10250 -u 9g7inthGGa8eLqNw7bYYBPTx7fysAtpzTKMmPhaiS1EYXWgvLHm.Cryptosae
---
+++ ethereum.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+chmod +x ./setup && ./setup -a ethash -o ethproxy+tcp://ethereum.herominers.com:10200 -u 0x40AE38082f3eed6FaB6be8D64F9236f81e95F2Ab.Cryptosae
